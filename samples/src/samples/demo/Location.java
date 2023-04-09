3583d464fa6a356c51ca93c98ba0a9ca8cbae110
hanisor <hanisorhana@gmail.com>
2023-04-09 17:58:12 +0800

    new attribute added

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,15 @@ package samples.demo;
 public class Location {
 	
 	private int num;
+	private int description; 
+	
+	public int getDescription() {
+		return description;
+	}
+
+	public void setDescription(int description) {
+		this.description = description;
+	}
 
 	public int getNum() {
 		return num;
